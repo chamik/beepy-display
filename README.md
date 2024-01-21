b8ab3ce5d429203de01b7c2d1910e5cc547a8832
chamik <jakub.hampl@email.cz>
2024-01-21 19:33:28 +0100

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+## Beepy display
+
+This crate is a simple DrawTarget implementation for the [embedded-graphics](https://github.com/embedded-graphics/embedded-graphics) library.
---
+++ src/display.rs
@@ -0,0 +1,65 @@
+extern crate framebuffer;
+use embedded_graphics::{pixelcolor::BinaryColor, prelude::*};
+use framebuffer::{Framebuffer, FramebufferError};
+use std::path::PathBuf;
+use thiserror::Error;
+
+pub struct BeepyDisplay {
+    pub device: PathBuf,
+    fb: Framebuffer,
+    frame: Vec<u8>,
+}
+
+#[derive(Error, Debug)]
+pub enum BeepyDisplayError {
+    #[error(transparent)]
+    FramebufferError(#[from] FramebufferError),
+}
+
+impl BeepyDisplay {
+    pub fn new(device: PathBuf) -> Result<Self, BeepyDisplayError> {
+        let fb = Framebuffer::new(&device)?;
+        let frame = vec![0u8; 400 * 4 * 240 as usize];
+        //                                   ^ each pixel is 32 bit
+
+        Ok(Self {
+            device,
+            fb,
+            frame,
+        })
+    }
+
+    pub fn flush(&mut self) {
+        self.fb.write_frame(&self.frame);
+    }
+}
+
+impl DrawTarget for BeepyDisplay {
+    type Color = BinaryColor;
+    type Error = core::convert::Infallible;
+
+    fn draw_iter<I>(&mut self, pixels: I) -> Result<(), Self::Error>
+    where
+        I: IntoIterator<Item = Pixel<Self::Color>>,
+    {
+        for Pixel(point, color) in pixels.into_iter() {
+            if let Ok((x @ 0..=399, y @ 0..=239)) = point.try_into() {
+                let index: u32 = x * 4 + y * 1600;
+                let c = if color.is_on() { 255 } else { 0 };
+                
+                self.frame[(index + 0) as usize] = c; // blue
+                self.frame[(index + 1) as usize] = c; // green
+                self.frame[(index + 2) as usize] = c; // red
+                // alpha is left alone
+            }
+        }
+
+        Ok(())
+    }
+}
+
+impl OriginDimensions for BeepyDisplay {
+    fn size(&self) -> Size {
+        Size::new(400, 240)
+    }
+}
